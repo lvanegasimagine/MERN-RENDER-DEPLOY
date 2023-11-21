8cd42526b236d49b99680402234cc0b905dfb511
lvanegasimagine <lvanegas1429@gmail.com>
2023-11-21 10:50:48 -0600

    function handleRequest

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,5 @@
 export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
+
 export const DB_USER = process.env.DB_USER || "postgres"
 export const DB_HOST = process.env.DB_HOST || "localhost"
 export const DB_NAME = process.env.DB_NAME || "postgres"
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,23 @@
 import './App.css'
 import { useState } from 'react';
 
-const URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000' 
+const URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000'
 function App() {
   const [result, setResult] = useState('')
+
+  const handleRequest = async () => {
+    const res = await fetch(`${URL}/ping`)
+    const data = await res.json()
+    setResult(data)
+  }
   return (
     <>
       <h1>Render App</h1>
       <div className="card">
-        <button onClick={async () => {
-          const res = await fetch(`${URL}/ping`)
-          const data = await res.json()
-          setResult(data)
-        }}>
+        <button onClick={handleRequest}>
           Users
-
         </button>
-          <pre>{JSON.stringify(result, null, 2)}</pre>
+        <pre>{JSON.stringify(result, null, 2)}</pre>
       </div>
     </>
   )
